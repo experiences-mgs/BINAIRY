101e5f981b25d44f50a3a3a4a6d8b93b2cc05553
Mahamadoumgs13 <77594703+Mahamadoumgs13@users.noreply.github.com>
2023-12-07 14:35:01 +0000

    Add files via upload

---
+++ bitwriter2/main.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <bitset>
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace std;
+
+int main()
+{
+	const double dec = pow(2, 24) - 1;
+	const int bit = 24;
+
+	for (int long n = 0; n <= dec; n++) {
+
+		std::cout << "#define" << " " << "B" << std::bitset<bit>(n) << " " << std::dec << n << endl;
+
+	}
+	system("PAUSE");
+	return 0;
+}
